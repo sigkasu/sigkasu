3b50d82c30e7ae516593872287f373573ade534b
sigkasu <86993931+sigkasu@users.noreply.github.com>
2021-12-22 14:54:49 +0900

    Add files via upload

---
+++ migrations/2021_12_21_033125_create_party_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePartyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('party', function (Blueprint $table) {
+            $table->id();
+            $table->string('party');
+            $table->string('image');
+            $table->string('url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('party');
+    }
+}
