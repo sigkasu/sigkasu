ba1780dfae46a40232f5f306bf753203e8d816d5
sigkasu <86993931+sigkasu@users.noreply.github.com>
2022-02-07 17:05:53 +0900

    Add files via upload

--- fapp/app/Http/Controllers/Auth/RegisterController.php
+++ fapp/app/Http/Controllers/Auth/RegisterController.php
@@ -53,7 +53,7 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'selection' => ['required', 'string'],
+            'selection' => ['required', 'string', 'not_in:指定なし'],
         ]);
     }
 
